f7000a8a37b663aac192ed4279dc4b77710af7f9
Mario Medina <mariomedinam@gmail.com>
2025-09-05 15:07:32 -0500

    Agrega la ruta para acceder a la vista PhotographyView mediante el ID de una fotografía en urls.py.

--- app_maps/urls.py
+++ app_maps/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("categories/<int:category_id>/", views.CategoryDetailView.as_view(), name="category-detail"),
     path("states/", views.StateView.as_view(), name="states"),
     path("incidents/", views.IncidentView.as_view(), name="incidents"),
+    path("incidents/photography/<int:id_photography>/", views.PhotographyView.as_view(), name="photographies"),
 ]
